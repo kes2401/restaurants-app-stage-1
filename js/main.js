39aae337758f00066526619137991f17689a52be
kes2401 <keithscully83@gmail.com>
2018-07-16 20:41:28 +0100

    FIX: add ARIA attributes to correct accessibility features

--- js/main.js
+++ js/main.js
@@ -177,6 +177,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label', 'View details of ' + restaurant.name);
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
